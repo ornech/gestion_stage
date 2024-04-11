9158b2f21e47108982cf71830ee45465cf3f9b5f
ornech <jean-francois.ornech@ac-poitiers.fr>
2024-04-11 14:52:23 +0200

    modif

--- vues/accueil.php
+++ vues/accueil.php
@@ -5,6 +5,5 @@
 
 
     <h2>Bienvenue sur la page d'accueil</h2>
-    <p>Ceci est le contenu de la page d'accueil.</p>
     <!-- Ajoutez du contenu supplémentaire spécifique à la page d'accueil -->
 </main>
