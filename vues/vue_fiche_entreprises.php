d13e697e205531f9136f4ec6280260f8b018e057
Astrick <astghik.manukyan@lycee-merleauponty.fr>
2024-05-22 18:08:29 +0200

    de bootsreap à bulma
    changement du style, impossible de recuperer des données

--- vues/vue_fiche_entreprises.php
+++ vues/vue_fiche_entreprises.php
@@ -1,5 +1,8 @@
 <?php
 require_once 'config/auth.php';
+var_dump($_GET);
+var_dump($ficheEntreprise);
+var_dump($idEntreprise);
 ?>
 <style>
         h2 {
@@ -12,37 +15,36 @@ require_once 'config/auth.php';
             display: block;
             width: 50%;
             height: 2px;
-            background-color: #ADD8E6; /* Couleur bleu clair */
+            background-color: #ADD8E6;
             margin: 10px auto 0;
         }
     </style>
 <?php
+
     // Vérifier si les détails de l'entreprise sont disponibles
-    if($ficheEntreprise) {
+    if(isset($_GET["idEntreprise"])) {
+        echo 'ca existe';
         // Afficher les détails de l'entreprise
 ?>
-<BR>
-        <h2><?= $ficheEntreprise->nomEntreprise ?></h2>
 
-        <div class="container">
-  <div class="row">
-    <div class="col-sm-5 col-md-6">
-        <p><strong><?= $ficheEntreprise->nomEntreprise ?></strong> </p>
-        <p><?= $ficheEntreprise->adresse ?><BR>
+       <h2><?= $ficheEntreprise->nomEntreprise ?></h2>
+       <div class="columns">
+  <div class="column">
+  <p><strong><?= $ficheEntreprise->nomEntreprise ?></strong> </p>
+        <p><?= $ficheEntreprise->adresse ?>
+
         <?= $ficheEntreprise->codePostal ?> <?= $ficheEntreprise->ville ?></p>
         <p><strong>Siret: </strong><?= $ficheEntreprise->siret ?></p>
         <p><strong>Activité</strong> <?= $ficheEntreprise->naf ?></p>
-        <p><strong>Effectif: </strong> <?= $ficheEntreprise->effectif ?></p>
+        <p><strong>Effectif: </strong> <?= $ficheEntreprise->effectif ? $ficheEntreprise->effectif: "test" ?></p>
         <p><strong>Ajouté par: </strong> <?= $ficheEntreprise->Created_User ?></p>
-    </div>
 
-
-    <div class="col-sm-5 offset-sm-2 col-md-6 offset-md-0"><iframe width="100%" height="300" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" id="gmap_canvas" src="https://maps.google.com/maps?width=500&amp;height=400&amp;hl=en&amp;q=<?= $ficheEntreprise->adresse, $ficheEntreprise->ville; ?>+()&amp;t=&amp;z=17&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe> <a href='http://maps-generator.com/fr'>Maps Generator FR</a> <script type='text/javascript' src='https://embedmaps.com/google-maps-authorization/script.js?id=9c9c1fccad93b580a064555d49b07ca94b2880c6'></script></div>
-    </div>
-
-
-
+  </div>
+  <div class="column">
+  <div class="col-sm-5 offset-sm-2 col-md-6 offset-md-0"><iframe width="100%" height="300" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" id="gmap_canvas" src="https://maps.google.com/maps?width=500&amp;height=400&amp;hl=en&amp;q=<?= $ficheEntreprise->adresse, $ficheEntreprise->ville; ?>+()&amp;t=&amp;z=17&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe> <a href='http://maps-generator.com/fr'>Maps Generator FR</a> <script type='text/javascript' src='https://embedmaps.com/google-maps-authorization/script.js?id=9c9c1fccad93b580a064555d49b07ca94b2880c6'></script></div>
+</div
 <?php
+
     } else {
         // Si aucune entreprise n'a été trouvée, afficher un message d'erreur
         echo "<p>Aucune entreprise trouvée avec cet identifiant.</p>";
