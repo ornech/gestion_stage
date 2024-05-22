fe07908bf657af9d8379e6cdc43736762577cfba
Logan Gaillard <logangaillard1@gmail.com>
2024-05-22 17:08:19 +0200

    Delete Bootstrap

--- password_reset.php
+++ password_reset.php
@@ -163,7 +163,6 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
      </div>
 
 </main>
-<script src="js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 
 </body>
 </html>
