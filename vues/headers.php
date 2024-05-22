97b43f4eeeb3b96cc03a8d4e4ce9f90485c275a6
Jean-François ORNECH <jean-francois.ornech@ac-poitiers.fr>
2024-05-22 10:29:24 +0200

    Modif header et navbar prof

--- vues/headers.php
+++ vues/headers.php
@@ -7,14 +7,16 @@ require_once 'config/auth.php';
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Application gestion de stages</title>
-  <!-- Bootstrap CSS -->
+  <!-- Bootstrap CSS
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+-->
   <!-- Font Awesome -->
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
 
-  <!-- Bulma.io --> 
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@1.0.0/css/bulma-rtl.min.css"
-  >
+  <!-- Bulma.io -->
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@1.0.0/css/bulma-rtl.min.css">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@1.0.0/css/bulma.min.css">
+  
   <script>
   function sortTable(colIndex) {
     var table = document.getElementById("maTable");
--- vues/navbar_prof.php
+++ vues/navbar_prof.php
@@ -1,38 +1,99 @@
 <?php
 require_once 'config/auth.php';
 ?>
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <div class="container">
-    <a class="navbar-brand" href="index.php">GESTION STAGES </a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav mr-auto">
-        <li class="nav-item active">
-          <a class="nav-link" href="router.php?page=listerEntreprises"><i class="fa-regular fa-address-card"></i> &nbsp; Entreprises</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="router.php?page=gestion_etu"><i class="fas fa-users"></i>&nbsp; Etudiants</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="router.php?page=stage_list"><i class='far fa-calendar-alt'>&nbsp;</i>Stages</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="router.php?page=recherche"><i class='fa fa-search'>&nbsp;</i>Recherche</a>
-        </li>
-      </ul>
-    </div>
-
-    <!-- Bouton de déconnexion aligné à droite -->
-    Professeur: &nbsp;<b><?php echo $_SESSION['utilisateur'] ?> </b>&nbsp;
-
-    <a href="logout.php" class="btn btn-outline-danger my-2 my-sm-0" type="button">
-      <i class="fas fa-sign-out-alt"></i> Déconnexion
+<nav class="navbar is-link" role="navigation" aria-label="main navigation">
+  <div class="navbar-brand">
+    <a class="navbar-item" href="#">
+      <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28" alt="Logo">
     </a>
 
+    <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+      <span aria-hidden="true"></span>
+      <span aria-hidden="true"></span>
+      <span aria-hidden="true"></span>
+    </a>
+  </div>
 
+  <div id="navbarBasicExample" class="navbar-menu">
+    <div class="navbar-start">
+      <div class="navbar-item has-dropdown is-hoverable">
+        <a class="navbar-link" href="router.php?page=stage_list"><i class='far fa-calendar-alt'>&nbsp;</i>Stages</a>
+        <div class="navbar-dropdown">
+          <a class="navbar-item">
+            Mes suivis
+          </a>
+          <a class="navbar-item">
+            SIO1
+          </a>
+          <a class="navbar-item">
+            SIO2
+          </a>
+        </div>
+      </div>
+
+
+      <a class="navbar-item" href="router.php?page=listerEntreprises"><i class="fa-regular fa-address-card"></i>&nbsp;Entreprises</a>
+      <a class="navbar-item" href="router.php?page=recherche"><i class='fa fa-search'>&nbsp;</i>Recherche</a>
+
+      <div class="navbar-item has-dropdown is-hoverable">
+        <a class="navbar-link"> <i class="fas fa-users"></i>&nbsp; Etudiants </a>
+        <div class="navbar-dropdown">
+          <a class="navbar-item">
+            Liste
+          </a>
+          <a class="navbar-item" href="router.php?page=gestion_etu">Gestion</a>
+          <a class="navbar-item">
+            Importation
+          </a>
+        </div>
+      </div>
+
+
+      <div class="navbar-item has-dropdown is-hoverable">
+        <a class="navbar-link">
+          Profil
+        </a>
+        <div class="navbar-dropdown">
+          <p class="navbar-item">Compte professeur</p>
+          <a class="navbar-item" href="router.php?page=profil">
+            <?php echo $_SESSION['utilisateur'] ?>
+          </a>
+
+          <hr class="navbar-divider">
+
+            <a href="logout.php" class="navbar-item" ><i class="fas fa-sign-out-alt"></i>Se déconnecter</a>
+
+
+
+
+        </div>
+      </div>
+    </div>
   </div>
 </nav>
+
+<script>
+document.addEventListener('DOMContentLoaded', () => {
+  // Récupérer tous les éléments "navbar-burger"
+  const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+
+  // Vérifier s'il y a des burgers
+  if ($navbarBurgers.length > 0) {
+    // Ajouter un événement de clic sur chacun d'entre eux
+    $navbarBurgers.forEach(el => {
+      el.addEventListener('click', () => {
+        // Récupérer la cible à partir de l'attribut "data-target"
+        const target = el.dataset.target;
+        const $target = document.getElementById(target);
+
+        // Basculer les classes "is-active" sur le "navbar-burger" et le "navbar-menu"
+        el.classList.toggle('is-active');
+        $target.classList.toggle('is-active');
+      });
+    });
+  }
+});
+</script>
+
+
 <main class="container">
