f95153466901deea4548215ccf8f728d91d149e8
Astrick <astghik.manukyan@lycee-merleauponty.fr>
2024-05-23 10:35:55 +0200

    ajoute de style
    ajoute de style de vue profil comme sur la page de contact entreprise

--- vues/vue_profil_user.php
+++ vues/vue_profil_user.php
@@ -7,17 +7,45 @@ if(($_GET["page"] == "view_profil" && $_SESSION['statut'] == "Professeur") || $_
 if($Profil) {
 // Afficher les détails du profi
 ?>
+ <style>
+        .card {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+        }
 
-<h2> Le profil de <?= $Profil->nom ?></h2>
+        .card-content {
+            text-align: center;
+        }
 
-<p><strong>Nom: </strong> <?= $Profil->nom ?></p>
-<p><strong>Prénom: </strong> <?= $Profil->prenom ?></p>
-<p><strong>Mail: </strong> <?= $Profil->email ? $Profil->email : "Non défini" ?></p>
-<p><strong>Login: </strong> <?= $Profil->login ? $Profil->login : "Non défini" ?></p>
-<p><strong>Statut: </strong> <?= $Profil->statut ? $Profil->statut : "Non défini"  ?></p>
-<p><strong>Promotion: </strong> <?= $Profil->promo ? $Profil->promo : "Non défini"  ?></p>
-<p><strong>Spécialité: </strong> <?= $Profil->spe ? $Profil->spe : "Non défini"  ?></p>
-        
+        .blue-line-bottom {
+            border-bottom: 2px solid #00d1b2;
+        }
+
+
+    </style>
+
+<body>
+<div class="container">
+        <div class="card">
+            <div class="card-content">
+                <div class="content">
+                <h2 class="title is-2 has-text-centered blue-line-bottom mb-4">Le profil de <?= $Profil->nom ?></h2>
+    <p class="card-text">Nom: <strong><?= $Profil->nom ?></strong> </p>
+    <p class="card-text">Prénom: <strong><?= $Profil->prenom ?></strong> </p>
+    <p class="card-text">Mail: <strong><?= $Profil->email ? $Profil->email : "Non défini" ?></strong> </p>
+    <p class="card-text">Login: <strong><?= $Profil->login ? $Profil->login : "Non défini" ?></strong> </p>
+    <p class="card-text">Groupe: <strong>TESTTTTT</strong> </p>
+    <p class="card-text">Statut: <strong><?= $Profil->statut ? $Profil->statut : "Non défini"  ?></strong> </p>
+    <p class="card-text">Promotion: <strong><?= $Profil->promo ? $Profil->promo : "Non défini"  ?></strong> </p>
+    <p class="card-text">Spécialité: <strong><?= $Profil->spe ? $Profil->spe : "Non défini"  ?></strong> </p>
+  </div>
+  </div>
+  </div>
+</div>
+</body>
+
+
 <?php
 } else {
    // Si aucun profil n'a été trouvée, afficher un message d'erreur
