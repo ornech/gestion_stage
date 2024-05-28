6780531b2330911c11ce7be2b4671d8733306fcb
Jean-François ORNECH <jean-francois.ornech@ac-poitiers.fr>
2024-05-28 11:16:06 +0200

    Ajout des consignes à l'attention des étudiants

---
+++ vues/vue_stage_consignes.php
@@ -0,0 +1,80 @@
+<?php
+require_once 'config/auth.php';
+?>
+
+<section class="section">
+  <div class="container">
+    <p class="title is-1">Consignes générales</p>
+    <p class="subtitle is-4">Stages de 1ère et 2nd année</p>
+
+    <div class="notification is-info is-light">
+      <p class="subtitle is-4">Notes préalables</p>
+      <div class="block">
+        <ol>
+          <li>La recherche de stage doit débuter <strong>au moins 6 mois</strong> avant la date de départ.</li>
+          <li>La recherche de stage relève de la seule responsabilité de l’étudiant.</li>
+          <li>Les étudiants n’ayant pas trouvé de stage à la date de début de la période prévue <strong>devront être présents au lycée</strong> pendant les horaires de cours habituels afin de poursuivre activement la recherche d’une entreprise d’accueil.</li>
+        </ol>
+      </div>
+    </div>
+
+    <div class="content">
+      <p class="title is-2">Convention de stage</p>
+      <div class="block">
+        <ol>
+          <li>La convention de stage constitue le document qui autorise l’étudiant à se rendre sur son lieu de stage.</li>
+          <li>Ce document doit être établi en 3 exemplaires (entreprise, lycée, stagiaire) et visé par toutes les parties <strong>AVANT</strong> le départ en stage.</li>
+        </ol>
+      </div>
+      <h1 class="subtitle is-3">Attestation de stage</h1>
+      <p>Modèle à télécharger sur la plateforme Moodle</p>
+    </div>
+
+    <div class="notification is-warning is-light">
+      <p><strong>Principe juridique :</strong></p>
+      <p>La convention de stage est le seul document opposable pour prétendre à d’éventuelles indemnités financières en cas d’accident du travail (y compris accidents de trajets).</p>
+    </div>
+
+    <p class="title is-2">Pendant la période de stage</p>
+    <div class="content">
+      <p>Services en ligne : plateforme Moodle</p>
+      <p><strong>1ère semaine :</strong> Renseignement des coordonnées précises de la personne responsable de l’encadrement de l’étudiant pendant le stage. Ces éléments facilitent la prise de contact par les enseignants pour l’organisation du suivi de stage (visite ou entretien téléphonique).</p>
+      <p><strong>Au cours du stage :</strong> Rédaction régulière du journal de stage.</p>
+      <p>Environ 2 semaines avant la fin de la période de stage, compléter et faire viser par le responsable de l’entreprise l’<strong>attestation de stage</strong>.</p>
+      <p>Cette attestation sera à déposer dès le retour de stage auprès de l’enseignant désigné.</p>
+    </div>
+
+    <div class="notification is-warning is-light">
+      <p><strong>Principe juridique :</strong></p>
+      <p>L’attestation de stage constitue le document d’examen attestant de la durée de stage réalisée. La délivrance du diplôme y est conditionnée.</p>
+    </div>
+
+    <p class="title is-2">Documents à rédiger – Épreuve E4</p>
+    <div class="content">
+      <p>L’épreuve E4 « Support et mise à disposition de services informatiques » a pour objectif la validation du bloc de compétences n°1 du BTS SIO.</p>
+      <p>Elle prend appui sur 2 documents à produire par le candidat :</p>
+      <ul>
+        <li>Tableau de synthèse des compétences du bloc → modèle à télécharger sur Moodle</li>
+        <li>Portfolio numérique en ligne dont le candidat a la responsabilité ; il intègre entre autres :</li>
+        <ul>
+          <li>Résumés des activités professionnelles associées aux compétences</li>
+          <li>Documentations des activités professionnelles (cahiers des charges, plans, schémas, notices, plannings, etc.)</li>
+        </ul>
+      </ul>
+    </div>
+
+    <p class="title is-2">Après la période de stage de 1ère année</p>
+    <div class="content">
+      <p>À la suite du stage de 1ère année, une journée d’entraînement à l’épreuve E4 est organisée. La présence à cette journée est obligatoire. Elle a lieu durant la deuxième quinzaine du mois de juin. Son objectif est d’évaluer les documents préparés ainsi que la présentation orale.</p>
+      <p>Préparation et attendus :</p>
+      <ul>
+        <li>Chaque étudiant dispose d’un temps individuel avec un membre de l’équipe enseignante, organisé comme suit :</li>
+        <ul>
+          <li>Présentation de 5 à 10 minutes par l’étudiant – à préparer dans l’esprit de l’épreuve E4</li>
+          <li>Échanges sous forme de questions/réponses</li>
+          <li>Conseils et remédiation concernant les documents présentés</li>
+        </ul>
+      </ul>
+    </div>
+  </div>
+</section>
