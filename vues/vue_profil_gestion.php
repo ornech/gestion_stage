0bfb2589b3417d60ee51582c36a39fd787e31dd5
Logan Gaillard <logangaillard1@gmail.com>
2024-05-28 12:24:54 +0200

    Changement du bouton créer un compte

--- vues/vue_profil_gestion.php
+++ vues/vue_profil_gestion.php
@@ -4,73 +4,84 @@ include 'test_groupe.php';
 ?>
 
 <?php
-function convertDateFormat($date) {
-    // Utiliser DateTime pour convertir le format
-    $dateTime = DateTime::createFromFormat('Y-m-d', $date);
-    if ($dateTime) {
-        return $dateTime->format('d/m/Y');
-    } else {
-        // Gérer l'erreur si le format de la date est incorrect
-        return false;
-    }
+function convertDateFormat($date)
+{
+  // Utiliser DateTime pour convertir le format
+  $dateTime = DateTime::createFromFormat('Y-m-d', $date);
+  if ($dateTime) {
+    return $dateTime->format('d/m/Y');
+  } else {
+    // Gérer l'erreur si le format de la date est incorrect
+    return false;
+  }
 }
-    // Vérifier si les détails du profil sont disponibles
-    if($_SESSION['statut'] == "Professeur") {
-      $dateActuelle = new DateTime();
+// Vérifier si les détails du profil sont disponibles
+if ($_SESSION['statut'] == "Professeur") {
+  $dateActuelle = new DateTime();
 
-      //$verif=verifgroupe($conn, $dateActuelle);
-        // Afficher les détails du profil
+  //$verif=verifgroupe($conn, $dateActuelle);
+  // Afficher les détails du profil
 ?>
-<p class="title is-2">Gestion utilisateurs</p>
-<p class="subtitle is-4">Admimistration des comptes</p>
 
-  <a class='btn btn-success' href='router.php?page=create_user' role='button'>Créer un compte</a>
-    <table class="table table-striped table-hover tableFilter" id="maTable">
-        <thead>
-            <tr class="table-secondary">
-              <th class="lineFilter" name="Utilisateur"></th>
-              <th class="lineFilter" name="Date d'entrée"></th>
-              <th class="lineFilter" name="Classe"></th>
-              <th class="lineFilter" name="Login"></th>
-              <th class="lineFilter" name="Spécialité"></th>
-              <th class="lineFilter" name="Statut"></th>
-              <th><span id ="mdp" class="hover-text">Reset password &#9432;</span></th>
-              <th><span id="acc" class="hover-text">Désactivé  &#9432;</span> </th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php foreach ($profils as $profil): ?>
-                  <tr style="cursor: pointer;" onclick="window.location.href = 'router.php?page=view_profil&id=<?= $profil->id ?>'">
-                    <td><?= $profil->nom ?> <?= $profil->prenom ?></td>
-                    <td><?= isset($profil->date_entree) ? convertDateFormat($profil->date_entree) : "Non définit"; ?></td>
-                    <td><?=$test=verifgroupe($profil, $conn, $dateActuelle)?></td>
-                    <td><?= $profil->login ?></td>
-                    <td><?= $profil->spe ?></td>
-                    <td><?= $profil->statut ?></td>
-                    <?php
-                      if ($profil->password_reset == "1"){echo "<td><a class='btn btn-warning'
+  <p class="title is-2">Gestion utilisateurs</p>
+  <p class="subtitle is-4">Admimistration des comptes</p>
+
+  <a class='button is-success' href='router.php?page=create_user' role='button'>Créer un compte</a>
+  <table class="table table-striped table-hover tableFilter" id="maTable">
+    <thead>
+      <tr class="table-secondary">
+        <th class="lineFilter" name="Utilisateur"></th>
+        <th class="lineFilter" name="Date d'entrée"></th>
+        <th class="lineFilter" name="Classe"></th>
+        <th class="lineFilter" name="Login"></th>
+        <th class="lineFilter" name="Spécialité"></th>
+        <th class="lineFilter" name="Statut"></th>
+        <th><span id="mdp" class="hover-text">Reset password &#9432;</span></th>
+        <th><span id="acc" class="hover-text">Désactivé &#9432;</span> </th>
+      </tr>
+    </thead>
+    <tbody>
+      <?php foreach ($profils as $profil) : ?>
+        <tr style="cursor: pointer;" onclick="window.location.href = 'router.php?page=view_profil&id=<?= $profil->id ?>'">
+          <td><?= $profil->nom ?> <?= $profil->prenom ?></td>
+          <td><?= isset($profil->date_entree) ? convertDateFormat($profil->date_entree) : "Non définit"; ?></td>
+          <td><?= $test = verifgroupe($profil, $conn, $dateActuelle) ?></td>
+          <td><?= $profil->login ?></td>
+          <td><?= $profil->spe ?></td>
+          <td><?= $profil->statut ?></td>
+          <?php
+          if ($profil->password_reset == "1") {
+            echo "<td><a class='btn btn-warning'
                         href='router.php?page=reset_password&idProfil=" .
-                        $profil->id . "' role='button'>En cours</a></td>";}
+              $profil->id . "' role='button'>En cours</a></td>";
+          }
 
-                      if ($profil->password_reset == "0"){echo "<td><a class='btn btn-success'
+          if ($profil->password_reset == "0") {
+            echo "<td><a class='btn btn-success'
                         href='router.php?page=reset_password&idProfil=" .
-                        $profil->id . "' role='button'>Reset</a></td>";}
+              $profil->id . "' role='button'>Reset</a></td>";
+          }
 
-                      if ($profil->inactif == 1){echo "<td><a class='btn btn-secondary'
+          if ($profil->inactif == 1) {
+            echo "<td><a class='btn btn-secondary'
                         href='router.php?page=profil_enable&idProfil=" .
-                        $profil->id . "' role='button'>Désactivé</a></td>";}
+              $profil->id . "' role='button'>Désactivé</a></td>";
+          }
 
-                      if ($profil->inactif == 0){echo "<td><a class='btn btn-success'
+          if ($profil->inactif == 0) {
+            echo "<td><a class='btn btn-success'
                         href='router.php?page=profil_disable&idProfil=" .
-                        $profil->id . "' role='button'>Actif</a></td>";}
-                      ?>
-                    <?php endforeach; ?>
-                  </tr>
-        </tbody>
-    </table>
-    <?php
-      } else {
-      // Si aucune entreprise n'a été trouvée, afficher un message d'erreur
+              $profil->id . "' role='button'>Actif</a></td>";
+          }
+          ?>
+        <?php endforeach; ?>
+        </tr>
+    </tbody>
+  </table>
 
-      }
-    ?>
+<?php
+} else {
+  // Si le statut de l'utilisateur connecté est n'est pas un professeur :
+
+}
+?>
