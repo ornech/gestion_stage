b25e7710521d28fc598df660b945db2d5eecd09f
Jean-François ORNECH <jean-francois.ornech@ac-poitiers.fr>
2024-06-04 16:10:25 +0200

    Fix vue log

--- vues/vue_logs.php
+++ vues/vue_logs.php
@@ -22,16 +22,15 @@ if(isset($logs)): ?>
     <tbody>
       <?php
       // Fetch user logs from the database
-
       // Loop through the logs and display them in the table
       foreach ($logs as $log):
         ?>
         <tr>
-          <td><a href="router.php?page=view_profil&id=<?=$log->idUser?>" target="_blank"><?=$log->nomUser?> <?=$log->prenomUser?></a></td>
+          <td><a href="router.php?page=view_profil&id=<?=$log->idUser?>" target="_blank"><?=$log->nomEtu?> <?=$log->prenomEtu?></a></td>
           <td><?=$log->logType?></td>
           <td><?= date('d/m/Y H:i:s', strtotime($log->date)) ?></td>
           <td><?=$log->pointGagne?></td>
-          <td><a href="router.php?page=view_logs_details&id=<?=$log->idLog?>" target="_blank" class="button is-primary">Détail</a></td>
+          <td><a href="router.php?page=view_logs_details&id=<?=$log->idActivite?>" target="_blank" class="button is-primary">Détail</a></td>
         </tr>
       <?php endforeach;?>
     </tbody>
