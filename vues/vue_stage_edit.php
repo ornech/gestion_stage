1ffc344b014d861fbbd852e6269ce68ea89513f4
Astrick <astghik.manukyan@lycee-merleauponty.fr>
2024-06-04 13:20:24 +0200

    changement de titre de vue edit stage
    changement le text et le style de titre

--- vues/vue_stage_edit.php
+++ vues/vue_stage_edit.php
@@ -1,6 +1,12 @@
 <?php
 require_once 'config/auth.php';
 ?>
+<style>
+   .orange-line-bottom {
+            border-bottom: 2px solid orange;
+        }
+        </style>
+
 
 <?php if(isset($Stage)): 
   function getWeeksDifference($dateDebut, $dateFin) {
@@ -33,7 +39,7 @@ require_once 'config/auth.php';
 <div class="container">
   <div class="columns is-centered">
     <div class="column is-three-quarters">
-      <h1 class="title is-2 has-text-centered">Modification un stage de <?= $Stage->EtudiantNom . " ". $Stage->EtudiantPrenom?></h1>
+      <h1 class="title is-2 has-text-centered orange-line-bottom mb-4">Stage <?= $Stage -> classe ?>:  <?= $Stage->EtudiantNom . " ". $Stage->EtudiantPrenom?></h1>
 
       <form action="/controller/stage_edit.php" method="post">
         <input type="hidden" id="idEtudiant" name="idEtudiant" value="<?= $Stage->idEtudiant?>">
