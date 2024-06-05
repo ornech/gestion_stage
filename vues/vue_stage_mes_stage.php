00100e14188486a11574720206563ff458078c94
Jean-François ORNECH <jean-francois.ornech@ac-poitiers.fr>
2024-06-05 18:27:18 +0200

    fix

--- vues/vue_stage_mes_stage.php
+++ vues/vue_stage_mes_stage.php
@@ -1,44 +1,48 @@
 <?php
 require_once 'config/auth.php';
 
-//ajouter session id =:id
-if ($_GET["page"] == "stage_read" || $_GET["page"] == "stage") {
+
+var_dump($stage);
 
   // Vérifier si les détails du profil sont disponibles
   if (isset($stage)) {
-    if ($_SESSION['statut'] == "Etudiant" && $idUser == $_SESSION["userID"]) {
-
-      var_dump($stage);
-?>
+    if ($_SESSION['statut'] == "Etudiant") {
+   // $_SESSION["userID"]
+   ?>
 
 <!-- --------------- DEBUT ANCIENNE VUE ----------------------  -->
 
 <?php
-setlocale(LC_TIME, 'fr_FR.UTF-8');  //pour l'affichange de mois en francais
-$fmt = new IntlDateFormatter('fr_FR', IntlDateFormatter::LONG, IntlDateFormatter::NONE, 'Europe/Paris', IntlDateFormatter::GREGORIAN);
-
-$dateDebut = new DateTime($stage->dateDebut);
-$dateFin = new DateTime($stage->dateFin);
-$difference = $dateDebut->diff($dateFin); // (dateFin - dateDebut) -> difference en jour
-$semaines = floor($difference->days / 7); // difference en jour divisé sur 7
-$debutFormat = $fmt->format($dateDebut); //affichage les mois en lettre
-$finFormat = $fmt->format($dateFin);
-$professeurs = $profilModel->list_by_professeur();
+// setlocale(LC_TIME, 'fr_FR.UTF-8');  //pour l'affichange de mois en francais
+// $fmt = new IntlDateFormatter('fr_FR', IntlDateFormatter::LONG, IntlDateFormatter::NONE, 'Europe/Paris', IntlDateFormatter::GREGORIAN);
+//
+// $dateDebut = new DateTime($stage->dateDebut);
+// $dateFin = new DateTime($stage->dateFin);
+// $difference = $dateDebut->diff($dateFin); // (dateFin - dateDebut) -> difference en jour
+// $semaines = floor($difference->days / 7); // difference en jour divisé sur 7
+// $debutFormat = $fmt->format($dateDebut); //affichage les mois en lettre
+// $finFormat = $fmt->format($dateFin);
+// $professeurs = $profilModel->list_by_professeur();
 
 ?>
-<!-- --------------- DEBUT NOUVELLE VUE ----------------------  -->
-
-
-
+<?php if ($stage[0]) {
+  // code...
+?>
+<div class="box">
+  <p class="title is-1">Stage <?= $stage[0]->classe ?></p>
+  <p class="subtitle is-3"><?= $stage[0]->EtudiantNom ?> <?= $stage[0]->EtudiantPrenom ?></p>
+</div>
+<?php } ?>
+<?php }
+ if ($stage[1] != null) {
+?>
+<div class="box">
+  <p class="title is-1">Stage <?= $stage[0]->classe ?></p>
+  <p class="subtitle is-3"><?= $stage[0]->EtudiantNom ?> <?= $stage[0]->EtudiantPrenom ?></p>
+</div>
 
   <?php
     }
-  } else {
-    // Si aucun profil n'a été trouvée, afficher un message d'erreur
-    echo "<p>Aucun profil trouvé avec ce lien.</p>";
-  }
-} else {
-  header("Location: ../router.php?page=profil");
-} //Fin de la vérification de si l'utilisateur est connecté en tant que prof
-//
+else { echo "aucun stage" ;}
+}
   ?>
