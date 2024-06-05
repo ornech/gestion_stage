d80cdf83d4876f59125b33373511c49b61cb1226
Jean-François ORNECH <jean-francois.ornech@ac-poitiers.fr>
2024-06-05 19:38:33 +0200

    Fix

--- vues/vue_stage_mes_stage.php
+++ vues/vue_stage_mes_stage.php
@@ -1,48 +1,58 @@
 <?php
 require_once 'config/auth.php';
 
-
-var_dump($stage);
-
-  // Vérifier si les détails du profil sont disponibles
-  if (isset($stage)) {
-    if ($_SESSION['statut'] == "Etudiant") {
-   // $_SESSION["userID"]
-   ?>
-
-<!-- --------------- DEBUT ANCIENNE VUE ----------------------  -->
-
-<?php
-// setlocale(LC_TIME, 'fr_FR.UTF-8');  //pour l'affichange de mois en francais
-// $fmt = new IntlDateFormatter('fr_FR', IntlDateFormatter::LONG, IntlDateFormatter::NONE, 'Europe/Paris', IntlDateFormatter::GREGORIAN);
-//
-// $dateDebut = new DateTime($stage->dateDebut);
-// $dateFin = new DateTime($stage->dateFin);
-// $difference = $dateDebut->diff($dateFin); // (dateFin - dateDebut) -> difference en jour
-// $semaines = floor($difference->days / 7); // difference en jour divisé sur 7
-// $debutFormat = $fmt->format($dateDebut); //affichage les mois en lettre
-// $finFormat = $fmt->format($dateFin);
-// $professeurs = $profilModel->list_by_professeur();
-
+// var_dump($stage);
+if (isset($stage)) {
+ if ($_SESSION['statut'] == "Etudiant") {
+   //var_dump($_SESSION);
 ?>
-<?php if ($stage[0]) {
-  // code...
+<p class="title is-1">Mes stages</p>
+<p class="subtitle is-3"><?=$_SESSION["utilisateur"] ?></p>
+
+<?php if (isset($stage[0])) {
+  setlocale(LC_TIME, 'fr_FR.UTF-8');  //pour l'affichange de mois en francais
+  $fmt = new IntlDateFormatter('fr_FR', IntlDateFormatter::LONG, IntlDateFormatter::NONE, 'Europe/Paris', IntlDateFormatter::GREGORIAN);
+
+  $dateDebut = new DateTime($stage[0]->dateDebut);
+  $dateFin = new DateTime($stage[0]->dateFin);
+  $difference = $dateDebut->diff($dateFin); // (dateFin - dateDebut) -> difference en jour
+  $semaines = floor($difference->days / 7); // difference en jour divisé sur 7
+  $debutFormat = $fmt->format($dateDebut); //affichage les mois en lettre
+  $finFormat = $fmt->format($dateFin);
 ?>
+
 <div class="box">
-  <p class="title is-1">Stage <?= $stage[0]->classe ?></p>
-  <p class="subtitle is-3"><?= $stage[0]->EtudiantNom ?> <?= $stage[0]->EtudiantPrenom ?></p>
+  <p class="title is-2">Stage <?= $stage[0]->classe ?></p>
+  <p>Entreprise: <a href="../router.php?page=fiche_entreprise&idEntreprise=<?= $stage[0]->idEntreprise ?>"><?= $stage[0]->Entreprise ?></a></p>
+  <p>Début du stage : <?php echo $debutFormat; ?></p>
+  <p>Durée : <?= $semaines ?> semaines</p>
+  <p>Fin de stage : <?php echo $finFormat; ?></p>
+  <p>  <a href="http://gestage.localhost/router.php?page=stage_read&id=<?= $stage[0]->idStage ?>&idEtudiant=<?= $stage[0]->idEtudiant ?>">+ de détails</a></p>
 </div>
 <?php } ?>
+
 <?php }
- if ($stage[1] != null) {
+ if (isset($stage[1])) {
+   setlocale(LC_TIME, 'fr_FR.UTF-8');  //pour l'affichange de mois en francais
+   $fmt = new IntlDateFormatter('fr_FR', IntlDateFormatter::LONG, IntlDateFormatter::NONE, 'Europe/Paris', IntlDateFormatter::GREGORIAN);
+
+   $dateDebut = new DateTime($stage[0]->dateDebut);
+   $dateFin = new DateTime($stage[0]->dateFin);
+   $difference = $dateDebut->diff($dateFin); // (dateFin - dateDebut) -> difference en jour
+   $semaines = floor($difference->days / 7); // difference en jour divisé sur 7
+   $debutFormat = $fmt->format($dateDebut); //affichage les mois en lettre
+   $finFormat = $fmt->format($dateFin);
 ?>
 <div class="box">
-  <p class="title is-1">Stage <?= $stage[0]->classe ?></p>
-  <p class="subtitle is-3"><?= $stage[0]->EtudiantNom ?> <?= $stage[0]->EtudiantPrenom ?></p>
+  <p class="title is-2">Stage <?= $stage[1]->classe ?></p>
+  <p>Entreprise: <a href="../router.php?page=fiche_entreprise&idEntreprise=<?= $stage[1]->idEntreprise ?>"><?= $stage[1]->Entreprise ?></a></p>
+  <p>Début du stage : <?php echo $debutFormat; ?></p>
+  <p>Durée : <?= $semaines ?> semaines</p>
+  <p>Fin de stage : <?php echo $finFormat; ?></p>
+  <p>  <a href="http://gestage.localhost/router.php?page=stage_read&id=<?= $stage[1]->idStage ?>&idEtudiant=<?= $stage[1]->idEtudiant ?>">+ de détails</a></p>
 </div>
 
-  <?php
+<?php
     }
-else { echo "aucun stage" ;}
 }
-  ?>
+?>
