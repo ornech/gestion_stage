2c080faccfc71bede6bd711f8ac73345765da795
Logan Gaillard <logangaillard1@gmail.com>
2024-06-05 16:28:35 +0200

    Push

---
+++ vues/vue_profil_edit.php
@@ -0,0 +1,39 @@
+<?php
+require_once 'config/auth.php';
+
+if($Profil):?>
+
+<div class="container">
+  <div class="columns is-centered">
+    <div class="column is-three-quarters">
+      <h1 class="title is-1 has-text-centered">Modification profil</h1>
+      <h1 class="subtitle is-3 has-text-centered"><?= $Profil->nom?> <?= $Profil->prenom ?></h1>
+
+      <form action="/controller/profil_edit.php" method="post">
+
+        <label class="label" for="email">Email</label>
+        <div class="field is-grouped">
+          <div class="control is-expanded">
+            <input type="text" class="input" placeholder="alicedupont@email.com" name="email" id="email" value=<?= $Profil->email?>>
+          </div>
+        </div>
+
+        <label class="label" for="email">Téléphone</label>
+        <div class="field is-grouped">
+          <div class="control is-expanded">
+            <input type="text" class="input" placeholder="06 00 00 00 00" name="telephone" id="telephone" value=<?= $Profil->telephone?>>
+          </div>
+        </div>
+
+        <div class="field">
+          <div class="control">
+            <button class="button is-primary" id="submitForm" disabled>Créer</button>
+          </div>
+        </div>
+
+      </form>
+    </div>
+  </div>
+</div>
+
+<?php endif;?>
