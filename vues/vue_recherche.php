778029f13842e69699ac2dd80be98687373dcc41
Astrick <astghik.manukyan@lycee-merleauponty.fr>
2024-06-06 12:00:13 +0200

    changement de vue recherche
    select input et button sont mis en html  avec bulma

--- vues/vue_recherche.php
+++ vues/vue_recherche.php
@@ -12,25 +12,32 @@ $selected_naf = isset($_POST['naf']) ? $_POST['naf'] : null;
 $selected_cp = isset($_POST['cp']) ? $_POST['cp'] : null;
 
 // HTML pour la liste déroulante
-echo '<form method="POST" action="router.php?page=recherche">';
-echo '<div class="input-group mb-3">';
-
-echo '<select class="form-select" name="naf" aria-label="Size 3 select example">';
-foreach ($tableau_naf as $code_naf => $libelle_naf) {
-    $selected = ($selected_naf === $code_naf) ? 'selected' : ''; // Vérifie si l'option doit être pré-sélectionnée
-    echo '<option value="' . $code_naf . '" ' . $selected . '>' . $libelle_naf . '</option>';
-    echo "\n";
-}
-echo '</select>';
-echo "\n";
-
-
-echo "&nbsp; <input id='cp' type='text' class='form-control' value ='" . $selected_cp . "' name='cp' placeholder='Code postal'>";
-echo "\n";
-
-echo '<input class="btn btn-outline-secondary" type="submit" value="Rechercher">';
-echo '</div>';
-echo '</form>';
-
 ?>
+
+
+<div class="box">
+    <form method="POST" action="router.php?page=recherche">
+        <div class="input-group mb-3">
+            <div class="select" style="margin-bottom: 10px;"> <!-- Ajoute de l'espace sous le select -->
+                <select name="naf" aria-label="Size 3 select example">
+                    <?php
+                    foreach ($tableau_naf as $code_naf => $libelle_naf) {
+                        $selected = ($selected_naf === $code_naf) ? 'selected' : '';
+                        echo '<option value="' . $code_naf . '" ' . $selected . '>' . $libelle_naf . '</option>';
+                        echo "\n";
+                    }
+                    ?>
+                </select>
+            </div>
+            <div class="field is-grouped" style="margin-bottom: 10px;">
+    <div class="control">
+        <input id="cp" type="text" class="input" name="cp" placeholder="Code postal" pattern="\d*" oninput="this.value=this.value.replace(/[^0-9]/g,'')">
+    </div>
+    <div class="control">
+        <button class="button" type="submit">Rechercher</button>
+    </div>
+</div>
+        </div>
+    </form>
+</div>
 <?php echo $resultat; ?>
