f7dac573c273811b5ffcceef59f30a3c9505bc1a
Logan Gaillard <logangaillard1@gmail.com>
2024-06-11 17:33:43 +0200

    Modifications majeur.
    Lorsqu'un professeur se connecte, un algorithme revoir tout les étudiant.
    Met à jour les classes si besoin.
    Supprime les étudiant datant de plus de 5 ans.
    
    Modification de gestion_etu pour que la classe soit echo depuis la base de données et pas d'un return.
    
    Déplacement et renommage de test_groupe en verif_étudiant dans controller.

--- controller/login.php
+++ controller/login.php
@@ -2,6 +2,7 @@
 require_once '../config/db_connection.php';
 
 include_once '../model/Login.php';
+include_once '../model/Profil.php';
 
 if(isset($_POST["login"]) && isset($_POST["password"])){ 
     $login = $_POST['login'];
@@ -14,8 +15,33 @@ if(isset($_POST["login"]) && isset($_POST["password"])){
         header("Location: /router.php?page=login&erreur=" . $connexion['message']);
         exit;
     }else{
-        header("Location: /router.php?page=accueil");
-        exit;
+      
+      if($_SESSION['statut'] == "Professeur"){
+        include_once 'verif_etudiant.php';
+        include_once 'delete_user.php';
+        
+        $profil = new Profil($conn);
+        $etudiants = $profil->list_by_etudiant();
+
+        foreach($etudiants as $etudiant){
+          verifEtu($etudiant, $conn);
+
+          //Si $etudiant->dateFirstConn est passé depuis plus de 5 ans ou plus, si oui on l'anonymise
+          if(isset($etudiant->dateFirstConn) && $etudiant->dateFirstConn != "" || $etudiant->dateFirstConn != null){
+            $dateFirstConn = new DateTime($etudiant->dateFirstConn);
+            $date_now = new DateTime();
+            $interval = $dateFirstConn->diff($date_now);
+            if($interval->y >= 5){
+              if(!anonymizeUser($etudiant->id)){
+                echo "error";
+              }
+            }
+          }
+        }
+      }
+
+      header("Location: /router.php?page=accueil");
+      exit;
     }
 
 }else{
