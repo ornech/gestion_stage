eb86cec8542baffcfeab6de288dc530fdd3f69ba
Astrick <astghik.manukyan@lycee-merleauponty.fr>
2024-06-18 12:01:58 +0200

    ajoute de localisation
    ajote de select des departaments, aussi function qui modifie le URL pour que ca affiche les resultats de departament qu'on veut

--- vues/vue_recherche.php
+++ vues/vue_recherche.php
@@ -73,20 +73,129 @@ $nafTable = [
             <?php } ?>
         </div>
     </div>
-    <div class="columns">
-        <div class="column is-full">
-            <div class="field is-grouped is-grouped-centered">
-                <div class="control">
-                    <button id="generateLink" class="button is-link" onclick="generateLink()">
-                        <span class="icon">
-                            <i class="fas fa-search"></i>
-                        </span>&nbsp;&nbsp;Recherche
-                    </button>
+
+
+<div class="columns">
+    <div class="column is-full">
+        <div class="field is-grouped">
+            <div class="control">
+                <div class="select">
+<select id="departements" name="departements">
+  <option value="Ain+(01)">Ain (01)</option>
+  <option value="Aisne+(02)">Aisne (02)</option>
+  <option value="Allier+(03)">Allier (03)</option>
+  <option value="Alpes-de-Haute-Provence+(04)">Alpes-de-Haute-Provence (04)</option>
+  <option value="Hautes-Alpes+(05)">Hautes-Alpes (05)</option>
+  <option value="Alpes-Maritimes+(06)">Alpes-Maritimes (06)</option>
+  <option value="Ardèche+(07)">Ardèche (07)</option>
+  <option value="Ardennes+(08)">Ardennes (08)</option>
+  <option value="Ariège+(09)">Ariège (09)</option>
+  <option value="Aube+(10)">Aube (10)</option>
+  <option value="Aude+(11)">Aude (11)</option>
+  <option value="Aveyron+(12)">Aveyron (12)</option>
+  <option value="Bouches-du-Rhône+(13)">Bouches-du-Rhône (13)</option>
+  <option value="Calvados+(14)">Calvados (14)</option>
+  <option value="Cantal+(15)">Cantal (15)</option>
+  <option value="Charente+(16)">Charente (16)</option>
+  <option value="Charente-Maritime+(17)" selected>Charente-Maritime (17)</option>
+  <option value="Cher+(18)">Cher (18)</option>
+  <option value="Corrèze+(19)">Corrèze (19)</option>
+  <option value="Corse-du-Sud+(2A)">Corse-du-Sud (2A)</option>
+  <option value="Haute-Corse+(2B)">Haute-Corse (2B)</option>
+  <option value="Côte-d'Or+(21)">Côte-d'Or (21)</option>
+  <option value="Côtes-d'Armor+(22)">Côtes-d'Armor (22)</option>
+  <option value="Creuse+(23)">Creuse (23)</option>
+  <option value="Dordogne+(24)">Dordogne (24)</option>
+  <option value="Doubs+(25)">Doubs (25)</option>
+  <option value="Drôme+(26)">Drôme (26)</option>
+  <option value="Eure+(27)">Eure (27)</option>
+  <option value="Eure-et-Loir+(28)">Eure-et-Loir (28)</option>
+  <option value="Finistère+(29)">Finistère (29)</option>
+  <option value="Gard+(30)">Gard (30)</option>
+  <option value="Haute-Garonne+(31)">Haute-Garonne (31)</option>
+  <option value="Gers+(32)">Gers (32)</option>
+  <option value="Gironde+(33)">Gironde (33)</option>
+  <option value="Hérault+(34)">Hérault (34)</option>
+  <option value="Ille-et-Vilaine+(35)">Ille-et-Vilaine (35)</option>
+  <option value="Indre+(36)">Indre (36)</option>
+  <option value="Indre-et-Loire+(37)">Indre-et-Loire (37)</option>
+  <option value="Isère+(38)">Isère (38)</option>
+  <option value="Jura+(39)">Jura (39)</option>
+  <option value="Landes+(40)">Landes (40)</option>
+  <option value="Loir-et-Cher+(41)">Loir-et-Cher (41)</option>
+  <option value="Loire+(42)">Loire (42)</option>
+  <option value="Haute-Loire+(43)">Haute-Loire (43)</option>
+  <option value="Loire-Atlantique+(44)">Loire-Atlantique (44)</option>
+  <option value="Loiret+(45)">Loiret (45)</option>
+  <option value="Lot+(46)">Lot (46)</option>
+  <option value="Lot-et-Garonne+(47)">Lot-et-Garonne (47)</option>
+  <option value="Lozère+(48)">Lozère (48)</option>
+  <option value="Maine-et-Loire+(49)">Maine-et-Loire (49)</option>
+  <option value="Manche+(50)">Manche (50)</option>
+  <option value="Marne+(51)">Marne (51)</option>
+  <option value="Haute-Marne+(52)">Haute-Marne (52)</option>
+  <option value="Mayenne+(53)">Mayenne (53)</option>
+  <option value="Meurthe-et-Moselle+(54)">Meurthe-et-Moselle (54)</option>
+  <option value="Meuse+(55)">Meuse (55)</option>
+  <option value="Morbihan+(56)">Morbihan (56)</option>
+  <option value="Moselle+(57)">Moselle (57)</option>
+  <option value="Nièvre+(58)">Nièvre (58)</option>
+  <option value="Nord+(59)">Nord (59)</option>
+  <option value="Oise+(60)">Oise (60)</option>
+  <option value="Orne+(61)">Orne (61)</option>
+  <option value="Pas-de-Calais+(62)">Pas-de-Calais (62)</option>
+  <option value="Puy-de-Dôme+(63)">Puy-de-Dôme (63)</option>
+  <option value="Pyrénées-Atlantiques+(64)">Pyrénées-Atlantiques (64)</option>
+  <option value="Hautes-Pyrénées+(65)">Hautes-Pyrénées (65)</option>
+  <option value="Pyrénées-Orientales+(66)">Pyrénées-Orientales (66)</option>
+  <option value="Bas-Rhin+(67)">Bas-Rhin (67)</option>
+  <option value="Haut-Rhin+(68)">Haut-Rhin (68)</option>
+  <option value="Rhône+(69)">Rhône (69)</option>
+  <option value="Haute-Saône+(70)">Haute-Saône (70)</option>
+  <option value="Saône-et-Loire+(71)">Saône-et-Loire (71)</option>
+  <option value="Sarthe+(72)">Sarthe (72)</option>
+  <option value="Savoie+(73)">Savoie (73)</option>
+  <option value="Haute-Savoie+(74)">Haute-Savoie (74)</option>
+  <option value="Paris+(75)">Paris (75)</option>
+  <option value="Seine-Maritime+(76)">Seine-Maritime (76)</option>
+  <option value="Seine-et-Marne+(77)">Seine-et-Marne (77)</option>
+  <option value="Yvelines+(78)">Yvelines (78)</option>
+  <option value="Deux-Sèvres+(79)">Deux-Sèvres (79)</option>
+  <option value="Somme+(80)">Somme (80)</option>
+  <option value="Tarn+(81)">Tarn (81)</option>
+  <option value="Tarn-et-Garonne+(82)">Tarn-et-Garonne (82)</option>
+  <option value="Var+(83)">Var (83)</option>
+  <option value="Vaucluse+(84)">Vaucluse (84)</option>
+  <option value="Vendée+(85)">Vendée (85)</option>
+  <option value="Vienne+(86)">Vienne (86)</option>
+  <option value="Haute-Vienne+(87)">Haute-Vienne (87)</option>
+  <option value="Vosges+(88)">Vosges (88)</option>
+  <option value="Territoire de Belfort+(90)">Territoire de Belfort (90)</option>
+  <option value="Essonne+(91)">Essonne (91)</option>
+  <option value="Hauts-de-Seine+(92)">Hauts-de-Seine (92)</option>
+  <option value="Seine-Saint-Denis+(93)">Seine-Saint-Denis (93)</option>
+  <option value="Val-de-Marne+(94)">Val-de-Marne (94)</option>
+  <option value="Val-d'Oise+(95)">Val-d'Oise (95)</option>
+  <option value="Guadeloupe+(971)">Guadeloupe (971)</option>
+  <option value="Martinique+(972)">Martinique (972)</option>
+  <option value="Guyane+(973)">Guyane (973)</option>
+  <option value="La Réunion+(974)">La Réunion (974)</option>
+  <option value="Mayotte+(976)">Mayotte (976)</option>
+</select>
+
                 </div>
+            </div>
+            <div class="control">
+                <button id="generateLink" class="button is-link" onclick="generateLink()">
+                    <span class="icon">
+                        <i class="fas fa-search"></i>
+                    </span>&nbsp;&nbsp;Recherche
+                </button>
             </div>
         </div>
     </div>
-</div> </div>
+</div>
+
 <script>
     function toggleCheckbox(row) {
         const checkbox = row.querySelector('input[type="checkbox"]');
@@ -106,14 +215,24 @@ $nafTable = [
         });
     }
 
-    function generateLink() {
-        const checkboxes = document.querySelectorAll('.naf-checkbox:checked');
-        const selectedNafs = Array.from(checkboxes).map(cb => cb.value);
-        
-        const baseUrl = 'https://annuaire-entreprises.data.gouv.fr/rechercher?terme=&cp_dep_label=Charente-Maritime+%2817%29&cp_dep_type=dep&cp_dep=17&fn=&n=&dmin=&dmax=&type=&label=&etat=&sap=&';
-        const additionalParams = '&nature_juridique=&tranche_effectif_salarie=&categorie_entreprise=';
-        const fullUrl = baseUrl + selectedNafs.map(naf => `naf=${naf}`).join('&') + additionalParams;
-        
-        window.open(fullUrl, '_blank');
-    }
+function generateLink() {
+    const checkboxes = document.querySelectorAll('.naf-checkbox:checked');
+    const selectedNafs = Array.from(checkboxes).map(cb => cb.value);
+    
+    const departementSelect = document.getElementById('departements');
+    const selectedDepartement = departementSelect.value;
+    const departementLabel = departementSelect.options[departementSelect.selectedIndex].text;
+    
+    // Extraire les deux premiers chiffres du code postal du département
+    const departementCode = selectedDepartement.substring(selectedDepartement.indexOf('(') + 1, selectedDepartement.indexOf(')')); 
+    
+    const baseUrl = 'https://annuaire-entreprises.data.gouv.fr/rechercher';
+    
+    // Construire l'URL avec les paramètres corrects
+    const fullUrl = `${baseUrl}?terme=&cp_dep_label=${encodeURIComponent(departementLabel)}%20${departementCode}&cp_dep_type=dep&cp_dep=${encodeURIComponent(departementCode)}&${selectedNafs.map(naf => `naf=${naf}`).join('&')}&fn=&n=&dmin=&dmax=&type=&label=&etat=&sap=`;
+    
+    // Ouvrir l'URL dans un nouvel onglet
+    window.open(fullUrl, '_blank');
+}
+
 </script>
