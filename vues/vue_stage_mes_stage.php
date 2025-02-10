9211eba027c20f19c0705fe7deccf42b9158ef4e
Logan Gaillard <logangaillard1@gmail.com>
2025-02-10 16:28:39 +0100

    Récupération de la convention par "mes stages"s

--- vues/vue_stage_mes_stage.php
+++ vues/vue_stage_mes_stage.php
@@ -1,11 +1,9 @@
 <?php
 require_once 'config/auth.php';
-
-// var_dump($stage);
-if (isset($stage)) {
- if ($_SESSION['statut'] == "Etudiant") {
-   //var_dump($_SESSION);
+if (isset($stage)) :
+ if ($_SESSION['statut'] == "Etudiant") :
 ?>
+
 <div class="columns is-vcentered">
   <div class="column">
     <p class="title is-1">Mes stages</p>
@@ -13,61 +11,42 @@ if (isset($stage)) {
 
   </div>
   <div class="column is-narrow">
-    <?php if (!isset($stage[0]) && !isset($stage[1]) || !isset($stage[1])) { ?>
+    <?php if (!isset($stage[0]) && !isset($stage[1]) || !isset($stage[1])): ?>
       <a href="../router.php?page=stage_create_etu" class="button"> 
-  <span class="icon has-text-primary">
-    <i class="fas fa-plus-circle"></i>
-  </span>
-  &nbsp;&nbsp;Ajouter un stage
-</a>
-    <?php } ?> 
+        <span class="icon has-text-primary">
+          <i class="fas fa-plus-circle"></i>
+        </span>
+        &nbsp;&nbsp;Ajouter un stage
+      </a>
+    <?php endif ?> 
   </div>
 </div>
 
-<?php if (isset($stage[0])) {
-  setlocale(LC_TIME, 'fr_FR.UTF-8');  //pour l'affichange de mois en francais
-  $fmt = new IntlDateFormatter('fr_FR', IntlDateFormatter::LONG, IntlDateFormatter::NONE, 'Europe/Paris', IntlDateFormatter::GREGORIAN);
+<?php endif;
+  foreach ($stage as $stageEtu):
+    setlocale(LC_TIME, 'fr_FR.UTF-8');  //pour l'affichange de mois en francais
+    $fmt = new IntlDateFormatter('fr_FR', IntlDateFormatter::LONG, IntlDateFormatter::NONE, 'Europe/Paris', IntlDateFormatter::GREGORIAN);
 
-  $dateDebut = new DateTime($stage[0]->dateDebut);
-  $dateFin = new DateTime($stage[0]->dateFin);
-  $difference = $dateDebut->diff($dateFin); // (dateFin - dateDebut) -> difference en jour
-  $semaines = floor($difference->days / 7); // difference en jour divisé sur 7
-  $debutFormat = $fmt->format($dateDebut); //affichage les mois en lettre
-  $finFormat = $fmt->format($dateFin);
-?>
-
-<div class="box">
-  <p class="title is-2">Stage <?= $stage[0]->classe ?></p>
-  <p>Entreprise: <a href="../router.php?page=fiche_entreprise&idEntreprise=<?= $stage[0]->idEntreprise ?>"><?= $stage[0]->Entreprise ?></a></p>
-  <p>Durée : <strong><?= $semaines ?> semaines </strong></p>
-  <p>Début du stage : <strong><?php echo $debutFormat; ?></strong></p>
-  <p>Fin de stage : <strong><?php echo $finFormat; ?></strong></p>
-  <p>  <a href="../router.php?page=stage_read&id=<?= $stage[0]->idStage ?>&idEtudiant=<?= $stage[0]->idEtudiant ?>">+ de détails</a></p>
-</div>
-<?php } ?>
-
-<?php }
- if (isset($stage[1])) {
-   setlocale(LC_TIME, 'fr_FR.UTF-8');  //pour l'affichange de mois en francais
-   $fmt = new IntlDateFormatter('fr_FR', IntlDateFormatter::LONG, IntlDateFormatter::NONE, 'Europe/Paris', IntlDateFormatter::GREGORIAN);
-
-   $dateDebut = new DateTime($stage[1]->dateDebut);
-   $dateFin = new DateTime($stage[1]->dateFin);
-   $difference = $dateDebut->diff($dateFin); // (dateFin - dateDebut) -> difference en jour
-   $semaines = floor($difference->days / 7); // difference en jour divisé sur 7
-   $debutFormat = $fmt->format($dateDebut); //affichage les mois en lettre
-   $finFormat = $fmt->format($dateFin);
+    $dateDebut = new DateTime($stageEtu->dateDebut);
+    $dateFin = new DateTime($stageEtu->dateFin);
+    $difference = $dateDebut->diff($dateFin); // (dateFin - dateDebut) -> difference en jour
+    $semaines = floor($difference->days / 7); // difference en jour divisé sur 7
+    $debutFormat = $fmt->format($dateDebut); //affichage les mois en lettre
+    $finFormat = $fmt->format($dateFin);
 ?>
 <div class="box">
-  <p class="title is-2">Stage <?= $stage[1]->classe ?></p>
-  <p>Entreprise: <a href="../router.php?page=fiche_entreprise&idEntreprise=<?= $stage[1]->idEntreprise ?>"><?= $stage[1]->Entreprise ?></a></p>
+  <p class="title is-2">Stage <?= $stageEtu->classe ?></p>
+  <p>Entreprise: <a href="../router.php?page=fiche_entreprise&idEntreprise=<?= $stageEtu->idEntreprise ?>"><?= $stageEtu->Entreprise ?></a></p>
   <p>Durée : <strong><?= $semaines ?> semaines</strong></p>
   <p>Début du stage : <strong><?php echo $debutFormat; ?></strong></p>
   <p>Fin de stage : <strong><?php echo $finFormat; ?></strong></p>
-  <p>  <a href="../router.php?page=stage_read&id=<?= $stage[1]->idStage ?>&idEtudiant=<?= $stage[1]->idEtudiant ?>">+ de détails</a></p>
+  <div class="buttons mt-1"></div>
+    <a href="../router.php?page=stage_convention&idStage=<?= $stageEtu->idStage ?>"><button class="button is-primary">Obtenir la convention</button></a>
+    <a href="../router.php?page=stage_read&id=<?= $stageEtu->idStage ?>&idEtudiant=<?= $stageEtu->idEtudiant ?>"><button class="button">Plus de détails</button></a>
+  </div>
 </div>
 
 <?php
-    }
-}
+endforeach;
+endif;
 ?>
