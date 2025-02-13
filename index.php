2ea4341bfb5bd3631a2ef51575070b6a23fcf0a1
Logan Gaillard <logangaillard1@gmail.com>
2025-02-13 09:27:53 +0100

    Correction possible

--- index.php
+++ index.php
@@ -1,16 +1,3 @@
 <?php
-// fichier index.php
-// Démarrer la session
-session_start();
-
-// Vérifier si l'utilisateur est connecté
-if (!isset($_SESSION['username']) && $page != 'login') {
-    // Rediriger vers la page de connexion
-    header("Location: /router.php?page=login");
-    exit;
-}
-
-// Inclure le routeur
-require_once 'router.php';
-
+header("Location: /router.php");
 ?>
